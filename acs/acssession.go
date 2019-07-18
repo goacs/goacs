51f51a2d80c3b15a23e7b808a6f1fd1eddc45596
Jakub Augustynowicz <github@augustynowicz.info>
2019-07-18 18:17:15 +0200

    corrected session return to valid pointer

--- acs/acssession.go
+++ acs/acssession.go
@@ -2,30 +2,31 @@ package acs
 
 import (
 	"fmt"
-	"goacs/acs/cpe"
 	"goacs/acs/xml"
+	"goacs/models/cpe"
 	"math/rand"
 	"net/http"
 	"strconv"
 	"time"
 )
 
-const SESSION_LIFETIME = 15
-const SESSION_GOROUTINE_TIMEOUT = 10
+const SessionLifetime = 15
+const SessionGoroutineTimeout = 10
 
 type ACSSession struct {
 	Id          string
 	IsNew       bool
+	IsBoot      bool
 	PrevReqType string
 	created_at  time.Time
 	cpe         cpe.CPE
 }
 
-var acsSessions map[string]ACSSession
+var acsSessions map[string]*ACSSession
 
 func StartSession() {
 	fmt.Println("acsSessions init")
-	acsSessions = make(map[string]ACSSession)
+	acsSessions = make(map[string]*ACSSession)
 	go removeOldSessions()
 }
 func CreateSession(request *http.Request, w http.ResponseWriter) (*ACSSession, http.ResponseWriter) {
@@ -33,7 +34,7 @@ func CreateSession(request *http.Request, w http.ResponseWriter) (*ACSSession, h
 	var sessionId = ""
 	cookie, err := request.Cookie("sessionId")
 
-	var session ACSSession
+	var session *ACSSession
 
 	if err != nil {
 		sessionId = generateSessionId()
@@ -42,24 +43,22 @@ func CreateSession(request *http.Request, w http.ResponseWriter) (*ACSSession, h
 	}
 
 	fmt.Println("Trying to retive session from memory " + sessionId)
-	memorySession, exist := acsSessions[sessionId]
+	_, exist := acsSessions[sessionId]
 
 	if exist == false {
 		fmt.Println("session non exist in memory")
-		fmt.Println("Creating new session " + session.Id)
+		fmt.Println("Creating new session " + sessionId)
 		session = createEmptySession(sessionId)
 	} else {
-		session = memorySession
+		session = acsSessions[sessionId]
 		fmt.Println("session exist in memory")
 		session.IsNew = false
 	}
 
-	newCookie := http.Cookie{Name: "sessionId", Value: sessionId, Expires: time.Now().Add(SESSION_LIFETIME * time.Second)}
-	//w.Header().Set("Set-Cookie", "sessionId="+session.Id)
+	newCookie := http.Cookie{Name: "sessionId", Value: sessionId, Expires: time.Now().Add(SessionLifetime * time.Second)}
 	http.SetCookie(w, &newCookie)
 
-	fmt.Println("returning session", session)
-	return &session, w
+	return session, w
 }
 
 func generateSessionId() string {
@@ -73,25 +72,26 @@ func printSessions() {
 	}
 }
 
-func createEmptySession(sessionId string) ACSSession {
+func createEmptySession(sessionId string) *ACSSession {
 	session := ACSSession{Id: sessionId, IsNew: true, created_at: time.Now()}
-	acsSessions[sessionId] = session
-	return session
+	acsSessions[sessionId] = &session
+	return acsSessions[sessionId]
 }
 
 func removeOldSessions() {
 	for {
 		now := time.Now()
 		for sessionId, session := range acsSessions {
-			if now.Sub(session.created_at).Minutes() > SESSION_LIFETIME {
+			if now.Sub(session.created_at).Minutes() > SessionLifetime {
 				fmt.Println("DELETING OLD SESSION " + sessionId)
 				delete(acsSessions, sessionId)
 			}
 		}
-		time.Sleep(SESSION_GOROUTINE_TIMEOUT * time.Second)
+		time.Sleep(SessionGoroutineTimeout * time.Second)
 	}
 }
 
 func (session *ACSSession) fillCPEFromInform(inform xml.Inform) {
 	session.cpe = cpe.CPE{}
+	session.IsBoot = inform.IsBootEvent()
 }
---
+++ models/cpe/cpe.go
@@ -0,0 +1,65 @@
+package cpe
+
+import (
+	"goacs/acs/xml"
+	"net"
+	"strings"
+)
+
+type CPE struct {
+	SerialNumber         string
+	OUI                  string
+	ProductClass         string
+	Manufacturer         string
+	SoftwareVersion      string
+	HardwareVersion      string
+	IpAddress            net.IPAddr
+	ConnectionRequestUrl string
+	Root                 string
+	ParametersInfo       []xml.ParameterInfo
+	ParameterValues      []xml.ParameterValue
+}
+
+func (cpe *CPE) AddParameterInfo(parameter xml.ParameterInfo) {
+	cpe.ParametersInfo = append(cpe.ParametersInfo, parameter)
+}
+
+func (cpe *CPE) AddParametersInfoFromResponse(parameters []xml.ParameterInfo) {
+	for _, parameter := range parameters {
+		cpe.AddParameterInfo(parameter)
+	}
+}
+
+func (cpe *CPE) AddParameter(parameter xml.ParameterValue) {
+	cpe.ParameterValues = append(cpe.ParameterValues, parameter)
+}
+
+func (cpe *CPE) AddParameterValuesFromResponse(parameters []xml.ParameterValue) {
+	for _, parameter := range parameters {
+		cpe.AddParameter(parameter)
+	}
+}
+
+func (cpe *CPE) GetFullPathParameterNames() []xml.ParameterInfo {
+	var filteredParameters []xml.ParameterInfo
+	for _, parameter := range cpe.ParametersInfo {
+		//check if last char in Name is not equal to . (dot)
+		if parameter.Name[len(parameter.Name)-1:] != "." {
+			filteredParameters = append(filteredParameters, parameter)
+		}
+	}
+
+	return filteredParameters
+}
+
+func DetermineDeviceTreeRootPath(parameters []xml.ParameterInfo) string {
+	for _, parameter := range parameters {
+		splittedParamName := strings.Split(parameter.Name, ".")
+
+		if splittedParamName[0] == "Device" { //
+			return "Device"
+		}
+	}
+
+	return "InternetGatewayDevice"
+}
