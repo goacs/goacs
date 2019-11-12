2a9fc4c3f5ca2d23bb1cb03e0c082613914f8102
jaugustynowicz <j.augustynowicz@multiplay.pl>
2019-11-12 23:17:30 +0100

    database structure

--- contrib/database/01_initial.sql
+++ contrib/database/01_initial.sql
@@ -9,8 +9,8 @@ create table cpe
 	connection_request_url varchar(255) null,
 	connection_request_user varchar(50) null,
 	connection_request_password varchar(64) null,
-	created_at datetime null,
-	updated_at datetime null
+	created_at datetime default CURRENT_TIMESTAMP not null,
+	updated_at datetime default CURRENT_TIMESTAMP not null
 );
 
 create index cpe_serial_number_index
@@ -22,3 +22,50 @@ create unique index cpe_uuid_uindex
 alter table cpe
 	add constraint cpe_pk
 		primary key (uuid);
+
+create table cpe_parameters
+(
+	cpe_uuid int null,
+	`key` varchar(255) null,
+	value varchar(255) null,
+	type varchar(16) null,
+	flags varchar(10) null,
+	created_at datetime default CURRENT_TIMESTAMP not null,
+	updated_at datetime default CURRENT_TIMESTAMP not null,
+	constraint cpe_parameters_pk
+		unique (cpe_uuid, `key`),
+	constraint cpe_parameters_cpe_uuid_key_uindex
+		unique (cpe_uuid, `key`)
+);
+
+create table templates
+(
+	id int auto_increment
+		primary key,
+	name varchar(50) null,
+	created_at datetime default CURRENT_TIMESTAMP null,
+	updated_at datetime default CURRENT_TIMESTAMP null
+);
+
+create table templates_parameters
+(
+	template_id int null,
+	`key` varchar(255) null,
+	value varchar(255) null,
+	type varchar(16) null,
+	flags varchar(10) null,
+	created_at datetime default CURRENT_TIMESTAMP not null,
+	updated_at datetime default CURRENT_TIMESTAMP not null,
+	constraint templates_parameters_pk
+		unique (template_id, `key`),
+	constraint templates_parameters_template_id_key_uindex
+		unique (template_id, `key`)
+);
+
+create table cpe_to_templates
+(
+	cpe_uuid varchar(36) not null,
+	template_id int not null,
+	constraint cpe_to_templates_pk
+		primary key (cpe_uuid, template_id)
+);
