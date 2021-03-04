9fc07c527c335bc6ffad5f299155c8dc43b64cd8
Jakub Augustynowicz <j.augustynowicz@multiplay.pl>
2021-03-04 11:29:23 +0100

    sets default product_class value on new device

--- app/ACS/Logic/ControllerLogic.php
+++ app/ACS/Logic/ControllerLogic.php
@@ -195,6 +195,7 @@ class ControllerLogic
             ],
             [
                 'oui' => $this->context->device->oui,
+                'product_class' => $this->context->parameterValues->get($this->context->device->root . "DeviceInfo.ProductClass")?->value ?? '',
                 'connection_request_url' => $this->context->parameterValues->get($this->context->device->root . "ManagementServer.ConnectionRequestURL")->value,
                 'updated_at' => now(),
             ]
