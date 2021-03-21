a0e966efced50c077609b157f8cebf83f7e838c5
Jakub Augustynowicz <github@augustynowicz.info>
2021-03-21 17:36:43 +0100

    typo

--- bin/install.sh
+++ bin/install.sh
@@ -2,10 +2,10 @@
 
 docker-compose up node
 docker-compose up -d goacs
-docker-composer exec acs composer install --no-dev
-docker-composer exec acs php artisan key:generate
-docker-composer exec acs php artisan jwt:secret --force
-docker-composer exec acs php artisan migrate:install
-docker-composer exec acs php artisan migrate
-docker-composer exec acs php artisan db:seed
-docker-composer exec acs supervisorctl restart goacs
+docker-compose exec acs composer install --no-dev
+docker-compose exec acs php artisan key:generate
+docker-compose exec acs php artisan jwt:secret --force
+docker-compose exec acs php artisan migrate:install
+docker-compose exec acs php artisan migrate
+docker-compose exec acs php artisan db:seed
+docker-compose exec acs supervisorctl restart goacs
--- bin/update.sh
+++ bin/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker-compose up node
-docker-composer exec acs composer install --no-dev
-docker-composer exec acs php artisan migrate
-docker-composer exec acs php artisan db:seed
-docker-composer exec acs supervisorctl restart goacs
+docker-compose exec acs composer install --no-dev
+docker-compose exec acs php artisan migrate
+docker-compose exec acs php artisan db:seed
+docker-compose exec acs supervisorctl restart goacs
